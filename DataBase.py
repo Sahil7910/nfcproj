63eddb883b47355354ed5d590eb9da4b45e8e015
Sahil7910 <sahilkinjalkar444@gmail.com>
2024-05-11 12:06:18 +0530

    Nfc Proj

---
+++ DataBase.py
@@ -0,0 +1,53 @@
+import mysql.connector
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, Sequence, inspect, select
+from sqlalchemy.orm import declarative_base
+
+
+engine = create_engine("mysql://root:admin@localhost/nfcreader",echo = True)
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    password = Column(String(50))
+
+
+class Members(Base):
+    __tablename__ = 'members'
+
+    id = Column(Integer, primary_key=True)
+    cardid = Column(String(50))
+    status  = Column(Integer)
+
+conn = engine.connect()
+
+
+# students = Table(
+#    'students', meta,
+#    Column('id', Integer, primary_key = True),
+#    Column('name', String(50),),
+#    Column('lastname', String(50)),
+# )
+#
+# meta.create_all(engine)
+
+
+# 'users', meta
+# ins = users.insert()
+# ins = users.insert().values(name = 'Ravi', password = 'Kapoor')
+
+# result = conn.execute(ins)
+
+# s = students.select()
+# conn = engine.connect()
+# result = conn.execute(s)
+#
+# for row in result:
+#    print (row)
+
+
+
